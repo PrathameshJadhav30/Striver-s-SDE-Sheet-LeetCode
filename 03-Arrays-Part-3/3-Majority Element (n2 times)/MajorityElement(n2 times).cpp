b0496c97a3478a8f4ae68ce43383a5128b78cc64
Prathamesh <prathameshaj2004@gmail.com>
2025-01-15 17:34:44 +0530

    LeetCode

---
+++ 03-Arrays-Part-3/3-Majority Element (n2 times)/MajorityElement(n2 times).cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        // Initialize variables:
+        // 'freq' keeps track of the frequency count.
+        // 'ans' stores the current candidate for the majority element.
+        int freq = 0, ans = 0;
+
+        // Iterate through the array to determine the majority element
+        for (int i = 0; i < nums.size(); i++) {
+            // If frequency count is zero, set the current element as the candidate
+            if (freq == 0) {
+                ans = nums[i];
+            }
+            // If the current element matches the candidate, increment the count
+            if (ans == nums[i]) {
+                freq++;
+            }
+            // Otherwise, decrement the count
+            else {
+                freq--;
+            }
+        }
+
+        // Return the majority element (guaranteed to exist per problem constraints)
+        return ans;
+    }
+};
